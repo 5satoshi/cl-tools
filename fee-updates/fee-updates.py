2359df9db79d47af92b772ded401352e548c5512
Simon <simon@honc.online>
2021-07-17 23:43:12 +0200

    no channels catch

--- fee-updates/fee-updates.py
+++ fee-updates/fee-updates.py
@@ -12,21 +12,22 @@ peers = l1.listpeers()
 dfp = pandas.DataFrame(peers["peers"])
 
 for i, row in dfp.iterrows():
-    channel_id = row["channels"][0]["channel_id"]
-    
-    msat_to_us = row["channels"][0]["msatoshi_to_us"]
-    msat_total = row["channels"][0]["msatoshi_total"]
-    
-    factor = 0.95
-    balance = (msat_to_us+1000000)/msat_total
-    new_fee = 128*pow(math.floor(1/(balance*factor)),2)-1
-    
-    ppm = row["channels"][0]["fee_proportional_millionths"]
-    
-    if ppm!=new_fee :
-        logging.info("Update fee:")
-        logging.info("Channel balance for " + channel_id + " is "+ str(balance) )
-        logging.info("Old ppm " + str(ppm) + "; new "+ str(new_fee) )
-        l1.setchannelfee(channel_id,0,new_fee)
+    if len(row["channels"])>0:
+        channel_id = row["channels"][0]["channel_id"]
         
-        time.sleep(5)
+        msat_to_us = row["channels"][0]["msatoshi_to_us"]
+        msat_total = row["channels"][0]["msatoshi_total"]
+        
+        factor = 0.95
+        balance = (msat_to_us+1000000)/msat_total
+        new_fee = 128*pow(math.floor(1/(balance*factor)),2)-1
+        
+        ppm = row["channels"][0]["fee_proportional_millionths"]
+        
+        if ppm!=new_fee :
+            logging.info("Update fee:")
+            logging.info("Channel balance for " + channel_id + " is "+ str(balance) )
+            logging.info("Old ppm " + str(ppm) + "; new "+ str(new_fee) )
+            l1.setchannelfee(channel_id,0,new_fee)
+            
+            time.sleep(5)
