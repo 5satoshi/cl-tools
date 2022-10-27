a5436089337e5c08c86c78867406e54432452b82
Simon <simon@honc.online>
2022-10-27 21:08:22 +0200

    add timelimit

--- fee-updates/forwards-updates.py
+++ fee-updates/forwards-updates.py
@@ -8,14 +8,14 @@ from datetime import datetime, date, timedelta
 import helper
 
 cfg_file = sys.argv[1]
-#cfg_file = "forwards.conf"
+#cfg_file = "forwardings.conf"
 
 log_config = helper.read_config("logging",cfg_file)
 logging.basicConfig(filename=os.environ['HOME']+'/'+log_config["path"], level=logging.INFO,format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filemode = 'a')
 
 l1 = LightningRpc(os.environ['HOME']+"/.lightning/bitcoin/lightning-rpc")
 
-forwards = l1.listforwards()
+forwards = l1.listforwards(timelimit=str(int(time.time())-60*60*24*7)+"000000000")
 
 dfp = pandas.DataFrame(forwards["forwards"])
 dfp["received_time"] = pandas.to_datetime(dfp["received_time"], unit = 's')
@@ -30,7 +30,6 @@ if db_config["database"]=="bq":
     
 else:
     # Create SQLAlchemy engine to connect to MySQL Database
-    engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-                                    .format(host=db_config["host"], db=db_config["database"], user=db_config["user"], pw=db_config["password"]))
+    engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=db_config["host"], db=db_config["database"], user=db_config["user"], pw=db_config["password"]))
     # Convert dataframe to sql table
     filtered_df.to_sql('forwardings', engine, index=False, if_exists="append")
