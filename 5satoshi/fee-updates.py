a8c40e4ed9ea8d30f3bdeb66c3eeaf6d09fe8ac5
Simon <simon@honc.online>
2023-05-02 11:07:48 +0200

    balance check added

--- 5satoshi/fee-updates.py
+++ 5satoshi/fee-updates.py
@@ -13,6 +13,7 @@ peers = l1.listpeers()
 dfp = pandas.DataFrame(peers["peers"])
 
 test =  False
+update_all = False
 
 for i, row in dfp.iterrows():
     if len(row["channels"])>0:
@@ -21,7 +22,7 @@ for i, row in dfp.iterrows():
         msat_to_us = row["channels"][0]["msatoshi_to_us"]
         msat_total = row["channels"][0]["msatoshi_total"]
         
-        balance = msat_to_us/msat_total
+        balance = (msat_to_us+1)/msat_total
         new_fee = pow(math.floor(1/balance),2)
         if new_fee>10000:
             new_fee=10000
@@ -33,7 +34,7 @@ for i, row in dfp.iterrows():
         if new_htlc_max < 0:
             new_htlc_max = msat_to_us
         
-        if msat_to_us - (0.1 * msat_total) > htlc_max  or msat_to_us < htlc_max:
+        if msat_to_us - (0.1 * msat_total) > htlc_max  or msat_to_us < htlc_max or update_all or balance>1:
             logging.info("Update fee:")
             logging.info("Channel balance for " + channel_id + " is "+ str(balance) )
             logging.info("Liquidity is now "+ str(msat_to_us) )
@@ -45,7 +46,6 @@ for i, row in dfp.iterrows():
                 print(channel_id + " feebase=", str(base_fee) + ",feeppm=" + str(new_fee) + ",htlcmax=" + str(new_htlc_max))
             
             time.sleep(5)
-
 ### db update -------------------------------------------------
 
 peers = l1.listpeers()
