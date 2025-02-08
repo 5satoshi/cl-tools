009c481f6739ef8ce39b7c65b0d7ae16ed5e9a22
Simon <simon@honc.online>
2025-02-08 23:36:14 +0100

    update for new cln version

--- 5satoshi/store-node-data.py
+++ 5satoshi/store-node-data.py
@@ -7,6 +7,30 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
+l1 = LightningRpc(os.environ['HOME']+"/.lightning/bitcoin/lightning-rpc")
+
+### Channels ---------------------------------------------
+
+channels = l1.listchannels()
+
+dfc = pandas.DataFrame(channels["channels"])
+dfc['last_update'] = pandas.to_datetime(dfc['last_update'], unit='s')
+
+dfc.to_gbq("lightning-fee-optimizer.version_1.channels",if_exists='replace')
+
+### Nodes ---------------------------------------------
+
+nodes = l1.listnodes()
+
+dfn = pandas.DataFrame(nodes["nodes"])
+dfn['last_timestamp'] = pandas.to_datetime(dfn['last_timestamp'], unit='s')
+
+dfn.to_gbq("lightning-fee-optimizer.version_1.nodes",if_exists='replace')
+
+
+### Forwardings --------------------------------------------
+
+
 status_query = """
     SELECT status, min(received_time) as mintime, max(received_time) as maxtime, min(created_index) as minindex, max(created_index) as maxindex
     FROM `lightning-fee-optimizer.version_1.forwardings` group by status
@@ -26,11 +50,12 @@ else:
         WHERE created_index >= """
     del_result = client.query(del_query + str(index_start))
 
-### Forwardings --------------------------------------------
 
-
-l1 = LightningRpc(os.environ['HOME']+"/.lightning/bitcoin/lightning-rpc")
 #index_start = 2692198
+forwards = l1.listforwards(index='created',start=int(index_start))
+dff = pandas.DataFrame(forwards["forwards"])
+dff["received_time"] = dff["received_time"]*10**6
+dff["resolved_time"] = dff["resolved_time"]*10**6
 
 #chunk_size=200000
 #all_fwds = []
@@ -42,40 +67,17 @@ l1 = LightningRpc(os.environ['HOME']+"/.lightning/bitcoin/lightning-rpc")
     #all_fwds = all_fwds + forwards["forwards"]
     #index_start = index_start + chunk_size
 
+### strange workaround to get the dtypes mapped
 event_query = """SELECT * FROM (SELECT * FROM `lightning-fee-optimizer.version_1.forwardings` where style is not null and failreason is not null LIMIT 1) A 
 UNION ALL 
 SELECT * FROM (SELECT * FROM `lightning-fee-optimizer.version_1.forwardings` where style is not null and updated_index is not null and resolved_time is not null LIMIT 1) B"""
 exmpl_evt = client.query(event_query).to_dataframe()
-exmpl_evt.received_time = exmpl_evt.received_time.astype(int)/10**9
-exmpl_evt.resolved_time = exmpl_evt.resolved_time.astype(int)/10**9
 
-forwards = l1.listforwards(index='created',start=int(index_start))
-all_fwds = exmpl_evt.to_dict('records') + forwards["forwards"]
+all_fwds = exmpl_evt.to_dict('records') + dff.to_dict('records')
 
-dff = pandas.DataFrame(all_fwds)
-dff["received_time"] = pandas.to_datetime(dff["received_time"], unit = 's')
-dff["resolved_time"] = pandas.to_datetime(dff["resolved_time"], unit = 's')
-
-dff.drop([0,1]).to_gbq("lightning-fee-optimizer.version_1.forwardings",if_exists='append')
+df = pandas.DataFrame(all_fwds)
+df.drop([0,1]).to_gbq("lightning-fee-optimizer.version_1.forwardings",if_exists='append')
 
 
-
-### Channels ---------------------------------------------
-
-channels = l1.listchannels()
-
-dfc = pandas.DataFrame(channels["channels"])
-dfc['last_update'] = pandas.to_datetime(dfc['last_update'], unit='s')
-
-dfc.to_gbq("lightning-fee-optimizer.version_1.channels",if_exists='replace')
-
-### Nodes ---------------------------------------------
-
-nodes = l1.listnodes()
-
-dfn = pandas.DataFrame(nodes["nodes"])
-dfn['last_timestamp'] = pandas.to_datetime(dfn['last_timestamp'], unit='s')
-
-dfn.to_gbq("lightning-fee-optimizer.version_1.nodes",if_exists='replace')
 
 
