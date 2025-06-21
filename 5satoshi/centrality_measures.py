2f852075b246fe2663e76df4fae70fc25df9a40b
Simon <simon@honc.online>
2025-06-21 07:42:19 +0200

    edge centrality measures added

--- 5satoshi/centrality_measures.py
+++ 5satoshi/centrality_measures.py
@@ -17,10 +17,12 @@ DG = nx.from_pandas_edgelist(channels[channels.active],"source","destination",ed
 tx_types = [("common",80000), ("micro",200), ("macro",4000000)]
 epsilon = 1
 
+# tx_type,tx_sat = tx_types[2]
+
 for tx_type,tx_sat in tx_types:
     #tx_sat = 4000000 #macro ~1000
-    #tx_sat = 80000 #common ~20
     #tx_sat = 200 #micro ~0.05
+    #tx_sat = 80000 #common ~20
     for source, dest, key, data in DG.out_edges(keys=True,data=True):
         a = DG[source][dest][key]['base_fee_millisatoshi']
         b = DG[source][dest][key]['fee_per_millionth']/1000000
@@ -38,7 +40,8 @@ for tx_type,tx_sat in tx_types:
     
     start = pd.Timestamp.now()
     
-    betweenness = nx.betweenness_centrality(newDG,normalized=True,weight='fee')
+    #betweenness = nx.betweenness_centrality(newDG,normalized=True,weight='fee')
+    betweenness = nx.edge_betweenness_centrality(newDG,normalized=True,weight='fee')
     
     stop = pd.Timestamp.now()
     
@@ -53,3 +56,24 @@ for tx_type,tx_sat in tx_types:
     nodescores["type"] = tx_type
     
     nodescores.to_gbq("lightning-fee-optimizer.version_1.betweenness",if_exists='append')
+    
+    ##### Edges
+    
+    start = pd.Timestamp.now()
+    
+    #betweenness = nx.betweenness_centrality(newDG,normalized=True,weight='fee')
+    edge_betweenness = nx.edge_betweenness_centrality(newDG,normalized=True,weight='fee')
+    
+    stop = pd.Timestamp.now()
+    
+    print('Time: ', stop - start) 
+    
+    edgescores = pd.DataFrame([(k[0],k[1],k[2],v) for k,v in edge_betweenness.items()], columns=['source','destination', 'key', 'shortest_path_share'])
+    
+    edgescores = pd.merge(edgescores, channels[channels.active], how="left", on=['source','destination'])
+    edgescores['rank'] = edgescores['shortest_path_share'].rank(method='min',ascending=False)
+    
+    edgescores["timestamp"] = max(channels["last_update"])
+    edgescores["type"] = tx_type
+    
+    edgescores.to_gbq("lightning-fee-optimizer.version_1.edge_betweenness",if_exists='replace')
